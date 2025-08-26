4d0e34e8db8a4395a2286bdcef34e72ab885b86e
Zach Winter <contact@zachwinter.com>
2025-08-26 12:34:58 -0400

    graph checkpoint

--- apps/cli/test-imports.js
+++
@@ -1,8 +0,0 @@
-import { analyzeToGraph, getCodeFiles, RustKuzuIngestor } from "@sage/analysis";
-const files = ["src/components/chat/Chat.tsx"];
-const analysisData = analyzeToGraph(files);
-const ingestor = new RustKuzuIngestor(".sage/code.kuzu");
-ingestor.initialize().then(async () => {
-  const result = await ingestor.ingestStream(analysisData);
-  console.log("Done:", result);
-});
